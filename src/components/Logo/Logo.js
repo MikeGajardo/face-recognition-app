28ec9c368949ec7abdb145c2279049b2713d4200
Michael Gajardo <105249375+MikeGajardo@users.noreply.github.com>
2023-02-07 20:16:00 -0500

    logo added inside of tilt

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import Tilt from 'react-parallax-tilt';
+import brain from './brain.png';
+import './Logo.css';
 
 const Logo = () => {
     return (
-        <div className='ma4 mt0'>
-            <Tilt>
-                <div style={{ height: '300px', backgroundColor: 'darkgreen' }}>
-                    <h1>React Parallax Tilt 👀</h1>
+            <div className='ma4 mt0'>
+            <Tilt className="Tilt br2 shadow-2" style={{ height: 150, width: 150}}>
+                <div className='inner-element pa4'>
+                    <img alt='brain logo' src={brain}/>
                 </div>
             </Tilt>
-        </div>
+            </div>
     );
 }
 
