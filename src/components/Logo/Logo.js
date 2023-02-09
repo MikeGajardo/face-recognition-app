2f18091d757cdb8673081fd18d7a3545ead642f3
Michael Gajardo <105249375+MikeGajardo@users.noreply.github.com>
2023-02-08 20:37:27 -0500

    gyroscope tilt added

--- src/components/Logo/Logo.js
+++ src/components/Logo/Logo.js
@@ -6,7 +6,7 @@ import './Logo.css';
 const Logo = () => {
     return (
         <div className='ma4 mt0'>
-            <Tilt className="Tilt br2 shadow-2" style={{ height: 150, width: 150 }}>
+            <Tilt gyroscope={true} tiltMaxAngleX={45} tiltMaxAngleY={45} className="Tilt br2 shadow-2" style={{ height: 150, width: 150 }}>
                 <div className='inner-element pa4'>
                     <img style={{ paddingTop: '5px' }} alt='brain logo' src={brain} />
                 </div>
