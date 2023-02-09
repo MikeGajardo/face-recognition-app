38cf1b6c2920e4d1e2ac6adbe2224763d24872fe
Michael Gajardo <105249375+MikeGajardo@users.noreply.github.com>
2023-02-08 21:05:53 -0500

    event handlers for text and submit added

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
@@ -6,17 +7,35 @@ import Rank from './components/Rank/Rank';
 import ParticlesBg from 'particles-bg';
 
 
-function App() {
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+    }
+  }
+
+  onInputChange = (event) => {
+    console.log(event.target.value);
+  }
+
+  onButtonSubmit = () => {
+    console.log('click')
+  }
+
+  render() {
   return (
     <div className="App">
       <ParticlesBg type="square" bg={true} />
       <Navigation />
       <Logo />
       <Rank />
-      <ImageLinkForm />
+      <ImageLinkForm onInputChange={this.onInputChange} onButtonSubmit={this.onButtonSubmit}/>
       {/* <FaceRecognition /> */}
     </div>
   );
 }
+}
+
 
 export default App;
